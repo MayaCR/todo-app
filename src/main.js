60eabd7db77fd297162c8d1ba35db17dec54f180
Kymia R. <106397404+MayaCR@users.noreply.github.com>
2024-08-14 10:33:25 -0400

    Change task style

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './styles/main.css'
+import { showInbox, showToday, showUpcoming } from './modules/updateView'
+
+const inboxBtn = document.querySelector('#inboxBtn')
+const todayBtn = document.querySelector('#todayBtn')
+const upcomingBtn = document.querySelector('#upcomingBtn')
+
+showInbox()
+
+inboxBtn.addEventListener('click', showInbox)
+todayBtn.addEventListener('click', showToday)
+upcomingBtn.addEventListener('click', showUpcoming)
